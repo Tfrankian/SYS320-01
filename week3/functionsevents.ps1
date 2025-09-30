4c2b8b2e606c2d68614859eb1fcf0497ce48dbd2
Tfrankian <trevor.frankian@mymail.champlain.edu>
2025-09-30 19:50:54 -0400

    week3

---
+++ week3/functionsevents.ps1
@@ -0,0 +1,81 @@
+﻿#Q1
+
+#Get-EventLog System -Source Microsoft-Windows-WinLogon
+
+#Q2
+
+#$loginouts = Get-EventLog System -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+
+#$loginoutsTable = @() #Empty the array to fill customly
+#for($i=0; $i -lt $loginouts.Count; $i++){
+#    $event = ""
+#    if($loginouts[$i].InstanceId -eq "7001") {$event="Logon"}
+#    if($loginouts[$i].InstanceId -eq "7002") {$event="Logoff"}
+
+#    $user = $loginouts[$i].ReplacementStrings[1]
+
+#    $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated;
+#                                           "id" = $loginouts[$i].InstanceId;
+#                                        "Event" = $event;
+#                                         "User" = $user | whoami;
+#                                         }
+#}
+#$loginoutsTable
+
+#Q3
+
+#$balls = Read-Host "how many days do you want to go back"
+
+#function scripting ($dayCount){    
+#    $loginouts = Get-EventLog system -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$dayCount)
+
+#    $loginoutsTable = @() 
+#    for($i=0; $i -lt $loginouts.Count; $i++){
+#        $event = ""
+#        if($loginouts[$i].InstanceId -eq "7001") {$event="Logon"}
+#        if($loginouts[$i].InstanceId -eq "7002") {$event="Logoff"}
+
+#        $user = $loginouts[$i].ReplacementStrings[1]
+
+#       $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated;
+#                                               "id" = $loginouts[$i].InstanceId;
+#                                            "Event" = $event;
+#                                             "User" = $user | whoami;
+#                                             }
+#    }
+#    $loginoutsTable
+#}
+#Scripting($balls)
+
+#Q4
+
+#$onoff = Get-EventLog system -source EventLog -After (Get-Date).AddDays(-40) | Where-Object {($_.EventId -eq "6005") -or ($_.EventId -eq "6006")}
+
+#$onofftable = @() 
+#for($i=0; $i -lt $onoff.Count; $i++){
+#    $event = ""
+#    if($onoff[$i].EventId -eq "6005") {$event="Start"}
+#    if($onoff[$i].EventId -eq "6006") {$event="Shutdown"}
+    
+#    $user = $onoff[$i].ReplacementStrings[1]
+
+#    $onofftable += [pscustomobject]@{"Time" = $onoff[$i].TimeGenerated;
+#                                        "id" = $onoff[$i].EventId;
+#                                     "Event" = $event;
+#                                      "User" = "System";
+#                                      }
+#}
+#$onofftable
+
+#Q5
+
+. (Join-Path $PSScriptRoot "idk.ps1")
+
+$Logouts = logs(15)
+$Logouts
+
+$shuts = shuts(25)
+$shuts
+
+$starts = starts(25)
+$starts
---
+++ week3/idk.ps1
@@ -0,0 +1,60 @@
+﻿function logs ($dayCount){    
+    $loginouts = Get-EventLog system -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$dayCount)
+
+    $loginoutsTable = @() 
+    for($i=0; $i -lt $loginouts.Count; $i++){
+        $event = ""
+        if($loginouts[$i].InstanceId -eq "7001") {$event="Logon"}
+        if($loginouts[$i].InstanceId -eq "7002") {$event="Logoff"}
+
+        $user = $loginouts[$i].ReplacementStrings[1]
+
+        $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated;
+                                               "id" = $loginouts[$i].InstanceId;
+                                            "Event" = $event;
+                                             "User" = $user | whoami;
+                                             }
+    }
+    $loginoutsTable
+}
+
+
+
+
+function shuts ($days){
+    $starts = Get-EventLog system -source EventLog -After (Get-Date).AddDays(-$days) | Where-Object {($_.EventId -eq "6006")}
+
+    $onofftable = @() 
+    for($i=0; $i -lt $starts.Count; $i++){
+        $event = ""
+        if($starts[$i].EventId -eq "6006") {$event="Shutdown"}
+    
+        $user = $starts[$i].ReplacementStrings[1]
+
+        $onofftable += [pscustomobject]@{"Time" = $starts[$i].TimeGenerated;
+                                            "id" = $starts[$i].EventId;
+                                         "Event" = $event;
+                                          "User" = "System";
+                                          }
+    }
+    $onofftable
+}
+
+function starts($days){
+    $starts = Get-EventLog system -source EventLog -After (Get-Date).AddDays(-$days) | Where-Object {($_.EventId -eq "6005")}
+
+    $onofftable = @() 
+    for($i=0; $i -lt $starts.Count; $i++){
+        $event = ""
+        if($starts[$i].EventId -eq "6005") {$event="Start"}
+    
+        $user = $starts[$i].ReplacementStrings[1]
+
+        $onofftable += [pscustomobject]@{"Time" = $starts[$i].TimeGenerated;
+                                            "id" = $starts[$i].EventId;
+                                         "Event" = $event;
+                                          "User" = "System";
+                                          }
+    }
+    $onofftable
+}
