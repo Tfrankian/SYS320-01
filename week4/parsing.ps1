c3a8b320d63bed2b182a80f340efccf1cf3580c5
Tfrankian <trevor.frankian@mymail.champlain.edu>
2025-09-30 21:34:16 -0400

    week4

---
+++ week4/parsing.ps1
@@ -0,0 +1,20 @@
+﻿function ApacheLogs(){
+    $logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+
+    for ($i=0; $i -lt $logsNotformatted.Count; $i++){
+        $words=$logsNotformatted[$i].Split(" ")
+
+        $tableRecords += [pscustomobject]@{ "IP" = $words[0];
+                                            "Time" = $words[3].Trim('[');
+                                            "Method" = $words[5].Trim('"');
+                                            "Page" = $words[6];
+                                            "Protocol" = $words[7];
+                                            "Response" = $words[8];
+                                            "Referrer" = $words[10];
+                                            "Client" = $words[11..($words.count)];}
+    }
+    return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
+$tableRecords = ApacheLogs
+$tableRecords | Format-Table -AutoSize -Wrap
