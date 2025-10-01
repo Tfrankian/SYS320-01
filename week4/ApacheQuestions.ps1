d1f50801710fb509585c6a1200f57a72b80b412f
Tfrankian <trevor.frankian@mymail.champlain.edu>
2025-09-30 21:06:54 -0400

    week4

---
+++ week4/ApacheQuestions.ps1
@@ -0,0 +1,26 @@
+﻿#Get-Content C:\xampp\apache\logs\access.log
+
+#Get-Content C:\xampp\apache\logs\access.log -tail 5
+
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 ',' 400 '
+
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+#$A = Get-ChildItem -Path "C:\xampp\apache\logs\*.log" | Select-String -Pattern 'error'
+#$A[-5..-1]
+
+#$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+#$regex = [regex] "([0-9]{1,3}\.){3}[0-9]{1,3}"
+
+#$ipsUnorganized = $regex.Matches($notfounds)
+
+#$ips = @()
+#for($i=0;$i -lt $ipsUnorganized.Count;$i++){
+#    $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+#}
+#$ips | Where-Object { $_.IP -ilike "10.*" }
+
+$ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+$counts = $ipsoftens | Group-Object IP
+$counts | Select-Object Count, Name
---
+++ week4/apachelogs.ps1
@@ -0,0 +1,19 @@
+﻿function Apachelogs ($page,$code,$name){
+    $logs = Get-Content C:\xampp\apache\logs\access.log | Select-String -Pattern "$page" | Select-String -Pattern "$code" | Select-String -Pattern "$name" | ForEach-Object { $_.Line }
+
+    $regex = [regex] "([0-9]{1,3}\.){3}[0-9]{1,3}"
+    
+    if($logs){
+    $joined = $logs -join "'n"
+    $ipsUnorganized = $regex.Matches($logs)
+    }
+
+    
+    $ips = @()
+    for($i=0;$i -lt $ipsUnorganized.Count;$i++){
+        $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+    }
+    $ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+    $counts = $ipsoftens | Group-Object IP | Select Name, Count
+    return $counts
+}
